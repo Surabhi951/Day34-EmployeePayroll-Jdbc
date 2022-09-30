9e98f5f3a880d532c46556241427ce5bf1d8d90a
Surabhi951 <surabhibhagat95@gmail.com>
2022-09-30 17:07:57 +0530

    updated salary

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -1,8 +1,6 @@
 package com.bridgelabz;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,5 +20,17 @@ public class EmployeePayrollService {
         } catch (Exception e) {
             throw new EmployeePayrollException();
         }
+    }
+
+    public int updateSalary(String name, double salary) throws SQLException {
+        Connection connection = JdbcConnection.connectToDatabase();
+        PreparedStatement preparedStatement = connection.prepareStatement("update employee_payroll set salary = ? where name = ?");
+        preparedStatement.setDouble(1,salary);
+        preparedStatement.setString(2,name);
+        int rowsAffected = preparedStatement.executeUpdate();
+        if (rowsAffected > 0) {
+            System.out.println("salary updated successfully!");
+        }
+        return rowsAffected;
     }
 }
