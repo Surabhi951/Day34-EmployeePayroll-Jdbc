dec5d90b8a755abdf7e8bf6d8ac6945c3ef512eb
Surabhi951 <surabhibhagat95@gmail.com>
2022-09-30 16:23:08 +0530

    Retrieved the employee payroll data from the database

--- src/main/java/com/bridgelabz/EmployeePayrollMain.java
+++ src/main/java/com/bridgelabz/EmployeePayrollMain.java
@@ -1,7 +1,8 @@
 package com.bridgelabz;
 
 public class EmployeePayrollMain {
-    public static void main(String[] args) {
-        JdbcConnection.connectToDatabase();
+    public static void main(String[] args) throws EmployeePayrollException {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        employeePayrollService.retrieveData().forEach((System.out::println));
     }
 }
